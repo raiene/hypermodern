d8e3773b7d7b70071a631561c88896455bc68bbf
Raiene <raienesousa@mpto.mp.br>
2020-04-12 16:00:19 -0300

    Add requests dependency and usage

--- src/hypermodern/console.py
+++ src/hypermodern/console.py
@@ -1,10 +1,25 @@
+import textwrap
+
 import click
+import requests
 
 from . import __version__
+
+
+API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
     """The hypermodern python project."""
-    click.echo("Hello worlders")
+
+    with requests.get(API_URL) as responses:
+        responses.raise_for_status()
+        data = responses.json()
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="red")
+    click.echo(textwrap.fill(extract))
