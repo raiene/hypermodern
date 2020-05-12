ee9a49f6a90ce43739eec98b12a1ff0bbef7cac2
Raiene <raienesousa@mpto.mp.br>
2020-05-12 10:52:25 -0300

    Using mock staff

--- src/hypermodern/console.py
+++ src/hypermodern/console.py
@@ -1,28 +1,28 @@
 import textwrap
 
 import click
-import requests
 
-from . import __version__
+from . import __version__, wikipedia
 
-
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """The hypermodern python project."""
 
-    try:
-        with requests.get(API_URL) as responses:
-            responses.raise_for_status()
-            data = responses.json()
+    data = wikipedia.random_page(language=language)
 
-        title = data["title"]
-        extract = data["extract"]
+    title = data["title"]
+    extract = data["extract"]
 
-        click.secho(title, fg="blue")
-        click.echo(textwrap.fill(extract))
-    except requests.exceptions.ConnectionError:
-        click.secho("Sorry, Wikipedia API is unreacheble", fg="red")
+    click.secho(title, fg="blue")
+    click.echo(textwrap.fill(extract))
