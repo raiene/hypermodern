1c855a2c64e30f9041f883cf19409a1723c09cee
Raiene <raienesousa@mpto.mp.br>
2020-05-29 17:32:52 -0300

    Add type annotations  [chapter 4].

--- src/hypermodern/console.py
+++ src/hypermodern/console.py
@@ -15,7 +15,7 @@ from . import __version__, wikipedia
     show_default=True,
 )
 @click.version_option(version=__version__)
-def main(language):
+def main(language: str):
     """The hypermodern python project."""
 
     data = wikipedia.random_page(language=language)
