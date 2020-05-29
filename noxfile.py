a75f341db4939efe522195c6bb69cfaed4c1ea5e
Raiene <raienesousa@mpto.mp.br>
2020-05-29 17:16:59 -0300

    Using mypy [chapter 4].

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 locations = "src", "tests", "noxfile.py"
 
 
-nox.options.sessions = "lint", "tests", "safety"
+nox.options.sessions = "lint", "tests", "mypy"
 
 
 @nox.session(python=["3.8", "3.7"])
@@ -66,3 +66,10 @@ def install_with_constraints(session, *args, **kwargs):
             external=True,
         )
         session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+@nox.session(python=["3.8", "3.7"])
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
